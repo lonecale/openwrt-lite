5e1a3ecc1e7b5309b3d5139933b90b603d46e788
lonecale <54177993+lonecale@users.noreply.github.com>
2024-12-23 01:31:47 +0800

    Update 06-custom.sh

--- openwrt/scripts/06-custom.sh
+++ openwrt/scripts/06-custom.sh
@@ -14,7 +14,7 @@ git clone https://$github/sbwml/packages_utils_lrzsz package/new/lrzsz
 # clean up feeds
 rm -rf feeds/luci/applications/{luci-app-smartdns}
 rm -rf feeds/packages/net/{adguardhome,smartdns}
-rm -rf package/new/extd/{adguardhome,luci-app-adguardhome,smartdns,luci-app-smartdns,luci-app-netdata}
+rm -rf package/new/extd/{adguardhome,luci-app-adguardhome,smartdns,luci-app-smartdns,netdata,luci-app-netdata,luci-app-argon-config}
 
 # rm -rf feeds/packages/net/smartdns feeds/luci/applications/luci-app-smartdns package/new/extd/smartdns package/new/extd/luci-app-smartdns
 # rm -rf feeds/packages/net/adguardhome package/new/extd/adguardhome package/new/extd/luci-app-adguardhome
