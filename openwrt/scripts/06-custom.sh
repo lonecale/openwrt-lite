b46bb3b41c3782665da63a61f6a4ff3388f0e76e
lonecale <54177993+lonecale@users.noreply.github.com>
2025-04-05 14:59:45 +0800

    Update 06-custom.sh

--- openwrt/scripts/06-custom.sh
+++ openwrt/scripts/06-custom.sh
@@ -15,10 +15,11 @@ git clone https://$github/sbwml/packages_utils_lrzsz package/new/lrzsz
 # rm -rf feeds/luci/applications/{luci-app-smartdns}
 # rm -rf feeds/packages/net/{adguardhome,smartdns}
 rm -rf package/new/extd/{adguardhome,luci-app-adguardhome,smartdns,luci-app-smartdns,netdata,luci-app-netdata,luci-app-argon-config,oaf,open-app-filter,luci-app-oaf}
+rm -rf package/new/lite/{naiveproxy}
 
 git clone https://$github/lonecale/openwrt-custom-packages package/new/custom-packages
 
-dirs=(smartdns adguardhome luci-app-adguardhome luci-app-smartdns luci-app-wechatpush luci-app-chatgpt-web luci-theme-kucat luci-app-advancedplus luci-app-netwizard luci-app-timecontrol lucky luci-app-lucky luci-app-syscontrol netdata-ssl luci-app-netdata luci-app-oaf)
+dirs=(smartdns adguardhome luci-app-adguardhome luci-app-smartdns luci-app-wechatpush luci-app-chatgpt-web luci-theme-kucat luci-app-advancedplus luci-app-netwizard luci-app-timecontrol lucky luci-app-lucky luci-app-syscontrol netdata-ssl luci-app-netdata luci-app-oaf naiveproxy)
 
 for dir in "${dirs[@]}"; do
   mv "package/new/custom-packages/$dir" "package/new/"
